21b99519e5416234f72a4251c1b13ca68e05b0fb
Rafaël Blais Masson <rafbmasson@gmail.com>
2011-12-01 16:37:35 -0500

    Be nicer in the JS template

--- src/domwrapper.js
+++ src/domwrapper.js
@@ -1,14 +1,35 @@
 ;(function(window, document, undefined) {
-  var $A = function(iterable) { return Array.prototype.slice.call(iterable, 0) }
+  var $A = function(iterable) { // turns any iterable into an array
+    return Array.prototype.slice.call(iterable, 0)
+  }
   var DomWrapper = function() { return this.initialize.apply(this, arguments) }
   DomWrapper.prototype = {
     initialize: function(arg1, arg2) {
+      // (iterable)
+      if ('length' in arg1) {
+        for (var e = 0, l = arg1.length; e < l; e++)
+          this[e] = arg1[e]
+        this.length = arg1.length
+
+      } else {
+        // single element
+        this.length = 1
+        this[0] = arg1
+      }
+
+      return this
     },
     splice: function() { // makes WebKit Inspector log DomWrapper objects like arrays (yay!)
       return Array.prototype.splice.apply(this, arguments)
     },
 
     each: function(callback) {
+      if (this.length > 1) // avoid loop if only one item
+        for (var i = 0, l = this.length; i < l; i++)
+          callback.call(this[i], i, this[i])
+      else if (this.length === 1)
+        callback.call(this[0], 0, this[0])
+      return this
     },
 
     // HTML
